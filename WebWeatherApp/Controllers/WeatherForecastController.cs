d883b0ef70f574eaf9106771533e42ce10ccff2c
Stanislavcode <stas.dryk@mail.ru>
2022-07-17 15:06:29 +0300

    First Download

---
+++ WebWeatherApp/Controllers/WeatherForecastController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebWeatherApp.Models;
+
+namespace WebWeatherApp.Controllers
+{
+    public class WeatherForecastController : Controller
+    {
+        [HttpGet]
+        public IActionResult SearchByCity()
+        {
+            var viewModel = new SearchByCity();
+            return View(viewModel);
+        }
+        public IActionResult SearchByCity(SearchByCity model)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("City", "WeatherForecast", new { city = model.CityName });
+            }
+            return View(model);
+        }
+    }
+}
---
+++ WebWeatherApp/Models/SearchByCity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebWeatherApp.Models
+{
+    public class SearchByCity
+    {
+        [Required(ErrorMessage = "Oops! The city was not found")]
+        [Display(Name = "City:")]
+        [StringLength(30, MinimumLength = 2, ErrorMessage = "Invalid Input, Lenght must be between  2 to 20 character")]
+        public string CityName { get; set; }
+
+        public string Name { get; set; }
+        [Display(Name = "Temp")]
+        public float Temp { get; set; }
+        [Display(Name = "Humidity")]
+        public double Humidity { get; set; }
+        [Display(Name = "Pressure")]
+        public double Pressure { get; set; }
+        [Display(Name = "Wind Speed")]
+        public float Wind { get; set; }
+        [Display(Name = "Weather Condition")]
+        public string Weather { get; set; }
+    }
+}
